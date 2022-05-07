abfd249890b13d7b41594b3f34cf180c11bd74cf
Phillip Kamps <phillip.kamps@gmail.com>
2022-05-07 11:02:11 -0600

    Initial commit

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -1,7 +1,7 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require 'pry'
-require './lib/nytimes'
+require "minitest/autorun"
+require "minitest/pride"
+require "pry"
+require "./lib/nytimes"
 
 class NytimesTest < Minitest::Test
   attr_reader :hash
@@ -11,28 +11,29 @@ class NytimesTest < Minitest::Test
   end
 
   def test_it_can_get_copyright
-    #Set your code to the local variable, "result"
+    result = hash[:copyright]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
-    #Set your code to the local variable, "result"
+    result = hash[:results]
 
-    assert result.is_a? (Array)
+    assert result.is_a?(Array)
     assert_equal 44, result.count
   end
 
   def test_it_can_get_all_stories_with_subsection_of_politics
-    #Set your code to the local variable, "result"
+    # binding.pry
+    result = hash[:results].select do |result|
+      result[:subsection] == "Politics"
+    end
 
-
-    assert result.is_a? (Array)
+    assert result.is_a?(Array)
     assert_equal 6, result.count
     assert_equal "Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight",
       result.first[:title]
     assert_equal "Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.",
       result.last[:title]
   end
-
 end
